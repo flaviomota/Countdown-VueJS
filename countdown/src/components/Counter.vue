1b073b829b8710b7bf1bbcc471ca3679817f4a57
fmota89 <flaviofilipemota@gmail.com>
2022-12-07 17:16:51 +0000

    Style

---
+++ countdown/src/components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section class="text-3xl flex justify-center content-ce
+    <div>
+
+    </div>nter flex-col mx-auto text-center"
+    >Buy Now</section>
+    <section class="flex text=6l justify-center content-center">
+      <div class="days nr-2 relative">
+        {{displayDays}}
+        <div class="label text-sm absolute bottom-0">days</div>
+      </div>
+      <span class="leading-snug">:</span>
+      <div class="hours mx-2 relative">
+        {{displayHours}}
+        <div class="label text-sm absolute bottom-0">hours</div>
+      </div>
+      <span class="leading-snug">:</span>
+      <div class="minutes mx-2 relative">
+        {{displayMinutes}}
+        <div class="label text-sm absolute bottom-0">minutes</div>
+      </div>
+      <span class="leading-snug">:</span>
+      <div class="seconds ml-2 relative">
+        <div>{{displaySeconds}}</div>
+        <div class="label text-sm absolute bottom-0">seconds</div>
+      </div>
+    </section> 
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      displayDays: 0,
+      displayHours: 0,
+      displayMinutes: 0,
+      displaySeconds: 0
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ countdown/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Hello World</h3>
+    <Counter/>
+  </div>
+</template>
+
+<script>
+  import Counter from '@/components/Counter.vue'
+  export default {
+    components: {
+      Counter
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
